0e3008d48233b13e909ebb6b95aaffc70608c552
ZinchenkoVadym <zinchenkovadym@gmail.com>
2021-01-14 23:18:40 +0200

    hw

--- todo-func/src/App.js
+++ todo-func/src/App.js
@@ -1,19 +1,49 @@
 import React from 'react'
-import TodoList from "./Todo/Todolist"
+import TodoList from './Todo/Todolist'
+import Context from "./context";
+import AddTodo from "./Todo/Addtodo";
+
+
 
 function App() {
-    let todos = [
-        {id: 1, completed: true, title: 'buy tv'},
-        {id: 2, completed: true, title: 'buy mobile'},
-        {id: 3, completed: true, title: 'buy oven'}
-    ];
+
+    let [todos, setTodos] = React.useState([
+        {id: 1, completed: false, title: 'buy tv'},
+        {id: 2, completed: false, title: 'buy mobile'},
+        {id: 3, completed: false, title: 'buy oven'}
+    ])
+
+    function toggleTodo (id) {
+       setTodos(todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        }))
+
+    }
+
+    function removeTodo (id) {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    function addTodo (title) {
+        setTodos(todos.concat([{
+            title,
+            id: Date.now(),
+            completed: false
+        }]))
+    }
 
     return (
-    <div className="wrapper">
-      <h1>Todo list</h1>
-        <TodoList todos={todos} />
-    </div>
-  );
+        <Context.Provider value={{removeTodo}}>
+        <div className='wrapper'>
+            <h1>Todo list</h1>
+            <AddTodo onCreate={addTodo} />
+            <TodoList todos={todos} onToggle={toggleTodo}/>
+        </div>
+        </Context.Provider>
+    )
 }
 
 export default App;
--- todo-func/src/Todo/Todolist.js
+++ todo-func/src/Todo/Todolist.js
@@ -2,26 +2,32 @@ import React from 'react'
 import TodoItem from "./Todoitem";
 import PropTypes from 'prop-types'
 
-function TodoList (props) {
+
+function TodoList(props) {
     let styles = {
         ul: {
             listStyle: 'none',
             margin: 0,
             padding: 0
-        }
+        },
     }
     return (
         <ul style={styles.ul}>
-            {props.todos.map((todo, index)=>{
-                return <TodoItem todo={todo} key={todo.id} index={index}/>
+            {props.todos.map(function (todo, index) {
+                return <TodoItem
+                    todo={todo}
+                    key={todo.id}
+                    index={index}
+                    onChange={props.onToggle}
+                    />
             })}
-
         </ul>
     );
 }
 
 TodoList.propTypes = {
-todos: PropTypes.arrayOf(PropTypes.object).isRequired
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
 }
 
 export default TodoList;
